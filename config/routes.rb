9b0bdb726a2845c3f31b38fd9310ee61f7f589b9
Ekansh Singhal <ekansin05@gmail.com>
2023-04-01 01:20:31 +0530

    i18n

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,17 @@ Rails.application.routes.draw do
 
   get 'sessions/create'
   get 'sessions/destroy'
+
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
   end
 
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 end
 
