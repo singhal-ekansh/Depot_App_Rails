5035a804cf349256dce79b578e25c1483924b481
Ekansh Singhal <ekansin05@gmail.com>
2023-04-05 17:40:23 +0530

    validation extentions

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,19 @@
+class UrlValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    record.errors.add(attribute,'must be a URL for GIF, JPG or PNG image') unless value =~ %r{\.(gif|jpg|png)\z}i
+  end 
+end
+
+class PriceValidator < ActiveModel::Validator
+  def validate(record)
+    price_value = record.send(:price)
+    discount_value = record.send(:discount_price)
+    if price_value && discount_value 
+      record.errors.add :price, 'must be greater than discount price' if price_value < discount_value
+    end
+  end
+end
+
 class Product < ApplicationRecord
 
   has_many :line_items
@@ -5,12 +21,30 @@ class Product < ApplicationRecord
   before_destroy :ensure_not_referenced_by_any_line_item 
 
   validates :title, :description, :image_url, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  # validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|png)\z}i,
-    message: 'must be a URL for GIF, JPG or PNG image.'
-    }
+  # validates :image_url, allow_blank: true, format: {
+  #   with: %r{\.(gif|jpg|png)\z}i,
+  #   message: 'must be a URL for GIF, JPG or PNG image.'
+  #   }
+
+  # validation extentions 
+  validates :permalink, uniqueness: true
+  validates :permalink, format: {
+    without: /[^A-Za-z0-9\-]/,
+    message: 'spacial character and space not allowed'
+  }
+  validates :permalink, format: {
+    with: /(\w+)-(\w+)-(\w+)/,
+    message: 'minimum 3 words, separated by hyphen'
+  }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }, allow_blank: true
+  validate :validate_description_length
+  validates :image_url, url:true
+  validates_with PriceValidator
+  validates :price, comparison: {greater_than: :discount_price, message: 'must be greater than discount price' }
+  
+
 
   private 
     def ensure_not_referenced_by_any_line_item
@@ -19,5 +53,11 @@ class Product < ApplicationRecord
         throw :abort
       end
     end
+
+    def validate_description_length
+      errors.add(:description, 'length should be between 5-10') unless description && description.split.size.between?(5,10)
+    end
+
 end
+
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ApplicationRecord
   after_destroy :ensure_an_admin_remains
   validates :name, presence: true, uniqueness: true
   has_secure_password
+  
+  # validation extentions
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: {
+    with: %r{.+@\w+\.com\z},
+    message: "invalid syntax"
+  }
 
   class Error < StandardError
   end
