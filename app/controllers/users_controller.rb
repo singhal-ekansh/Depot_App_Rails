5dfb3fa49945b2d406858d1dfde7f2187f9cb0a2
Ekansh Singhal <ekansin05@gmail.com>
2023-04-06 18:18:23 +0530

    mailer changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to user_url, notice: "User #{@user.name} was successfully created." }
+        format.html { redirect_to @user, notice: "User #{@user.name} was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,7 +6,7 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.received.subject
   #
-  def received
+  def received(order)
     @order = order
     mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
@@ -16,7 +16,7 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.shipped.subject
   #
-  def shipped
+  def shipped(order)
     @order = order
     mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
