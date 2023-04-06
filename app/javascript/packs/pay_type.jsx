5f9ef4c02bdefaa0e7c23be0c18e972f7c886470
Ekansh Singhal <ekansin05@gmail.com>
2023-04-06 18:18:23 +0530

    mailer changes

--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -6,3 +6,4 @@ import PayTypeSelector from 'PayTypeSelector'     // (3)
   var element = document.getElementById("pay-type-component"); // (5)
   ReactDOM.render(<PayTypeSelector />, element);                 // (6)
 // });
+
